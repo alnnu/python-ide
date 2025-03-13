baa52e62d81c4827383be9bf6af6eafc22db5746
alnnu <luannf1020@gmail.com>
2025-03-13 10:44:23 -0300

    add: console via buffer

--- src/js/main.js
+++ src/js/main.js
@@ -7,20 +7,13 @@ const  clearButtomn = document.getElementById("clearButton")
 const worker = new Worker("./src/js/workers/worker.js")
 
 const interruptBuffer = new Uint8Array(new SharedArrayBuffer(16));
+const output = new Int32Array(new SharedArrayBuffer(2056));
 
-let message = {cmd: "load", interruptBuffer}
+let message = {cmd: "load", interruptBuffer,output}
 
 worker.postMessage(message)
 
-function addToOutput(value) {
-    for (const valueElement of value) {
-        if(consoleTab.getHTML() === "") {
-            consoleTab.innerHTML += ">>> " + valueElement
-        } else {
-            consoleTab.innerHTML += "<br/>>>> " + valueElement
-        }
-    }
-}
+
 
 runButtom.addEventListener("click", async () => {
 
@@ -29,32 +22,53 @@ runButtom.addEventListener("click", async () => {
         Atomics.store(interruptBuffer, 0, 0)
 
         runButtom.value = "stop"
-        message = { cmd: "run", code: editorTab.state.doc.toString()}
+
+        message = { cmd: "run", code: editorTab.state.doc.toString(), output}
+
         worker.postMessage(message)
 
     }else if (runButtom.value === "stop") {
 
         Atomics.store(interruptBuffer, 0, 2)
 
-        addToOutput(["code execution is stopping"])
+        runButtom.value = "stopping"
 
         runButtom.disabled = true
-
-        runButtom.value = "stopping"
 
     }else {
         alert("wait to the compiler to be read")
     }
 })
 
-worker.onmessage = (e) => {
-    addToOutput(e.data)
-    runButtom.value = "run"
+function readStringFromBuffer(uint32Array) {
+    const decoder = new TextDecoder()
 
-    runButtom.disabled = false
+    consoleTab.innerHTML += ">>> " + decoder.decode(uint32Array.slice(0, uint32Array.length - 1)) + "<br/>"
+
+
+    Atomics.store(output,uint32Array.length - 1 , 0);
+
+    Atomics.notify(output,uint32Array.length - 1)
+}
+
+
+worker.onmessage = (e) => {
+
+    readStringFromBuffer(output)
+
+
+    if(e.data === "finished" || e.data === "loaded") {
+        runButtom.value = "run"
+
+        runButtom.disabled = false
+    }
+
+
+
 }
 
 clearButtomn.addEventListener("click", () => {
     consoleTab.innerHTML = ""
 })
+
 
--- src/js/workers/worker.js
+++ src/js/workers/worker.js
@@ -1,10 +1,30 @@
-self.importScripts("https://cdn.jsdelivr.net/pyodide/v0.27.2/full/pyodide.js");
+self.importScripts("https://cdn.jsdelivr.net/pyodide/v0.27.2/full/pyodide.js")
 
 let pyCompiler
 
 async function CreateCompiler(interruptBuffer) {
     pyCompiler = await loadPyodide()
-    pyCompiler.setInterruptBuffer(interruptBuffer);
+    pyCompiler.setInterruptBuffer(interruptBuffer)
+}
+
+function writeStringToBuffer(str, uint32Array) {
+    const encoder = new TextEncoder()
+    const encoded = encoder.encode(str)
+
+    if (encoded.length > uint32Array.length) {
+        throw new Error("String To big")
+    }
+
+
+    Atomics.wait(uint32Array, uint32Array.length - 1, 1)
+
+    Atomics.store(uint32Array, uint32Array.length - 1, 1)
+
+    for (let i = 0; i < uint32Array.length - 1; i++) {
+        Atomics.store(uint32Array, i, encoded[i]);
+    }
+
+
 }
 
 self.addEventListener('message', async (e) => {
@@ -14,25 +34,31 @@ self.addEventListener('message', async (e) => {
     if (data.cmd === "load") {
 
         await CreateCompiler(data.interruptBuffer);
-        postMessage(["read to run"])
+
+
+        writeStringToBuffer("read to run", data.output)
+
+        postMessage("loaded")
 
     }else if(data.cmd === "run"){
         async function runPy(code) {
-            const arr = []
             try {
-                pyCompiler.setStdout({ batched: (msg) => arr.push(msg)})
+                pyCompiler.setStdout({ batched: (msg) => {
+                        writeStringToBuffer(msg, data.output)
+                        postMessage("log")
+                    }})
 
                 pyCompiler.runPython(code)
 
             } catch (err) {
-                arr.push(err.toString())
+                writeStringToBuffer(err.toString(), data.output)
+                postMessage("error")
             }
-            return(arr)
         }
 
-        let output = await runPy(data.code)
+        await runPy(data.code)
 
-        postMessage(output)
+        postMessage("finished")
     }
 })
 
